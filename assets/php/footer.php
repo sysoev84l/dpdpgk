80f8d0a637b2a8bcac67d100359db678be8e15db
sysoev84l <sysoev84l@ya.ru>
2024-01-17 21:11:35 +0300

    v 1.0.1

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -1,21 +1,16 @@
 <footer>
-	<div class="footer-top">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+
+				<div class="footer-top">
 					<h2>
 						<a href="https://dpd.ru" target="_blank" rel="noopener noreferrer">
 							<img src="./assets/img/dpd2.jpg" alt="" class="footer-img">
 						</a>
 					</h2>
 				</div>
-			</div>
-		</div>
-	</div>
-	<div class="footer-bottom">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
+				<div class="footer-bottom">
 					<div class="footer-bottom__wrapper">
 						<div class="img-foooter_wrap">
 							<a href="https://dpd.ru" target="_blank" rel="noopener noreferrer">
