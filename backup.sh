46f752dcf2630a5b43333b892582ea9f9a7d512f
sysoev84l <sysoev84l@ya.ru>
2024-01-17 20:33:32 +0300

    v 1.0.1

---
+++ backup.sh
@@ -0,0 +1,13 @@
+# export sql
+./export.php
+./export2.php
+
+# git commit & push to github
+git add .
+git commit -m 'v 1.0.1'
+git push
+
+# archiving and saving to yandex.disk
+tar -cvzf ../dpdpgk.local.tar.gz *
+zip -r ../dpdpgk.local.zip *
+cp ../dpdpgk.local.* ~/Yandex.Disk/_backup/www/
--- git_push.sh
+++
@@ -1,5 +0,0 @@
-./export.php
-./export2.php
-git add .
-git commit -m 'v 1.0.1'
-git push
