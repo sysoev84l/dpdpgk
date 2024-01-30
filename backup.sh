84378318b212f7533157890a38c6bdc4ff9af704
sysoev84l <sysoev84l@ya.ru>
2024-01-30 20:02:06 +0300

    v 1.0.1

--- backup.sh
+++ backup.sh
@@ -8,7 +8,10 @@ git add .
 git commit -m 'v 1.0.1'
 git push
 
-# archiving and saving to yandex.disk
+# archiving and copy  to yandex.disk folders
 tar -cvzf ../dpdpgk.local.tar.gz *
 zip -r ../dpdpgk.local.zip *
 cp --verbose  ../dpdpgk.local.* ~/Yandex.Disk/_backup/www/
+
+# sync yandex.disk
+yandex-disk sync
