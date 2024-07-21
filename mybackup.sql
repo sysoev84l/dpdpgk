d0ed344434013a9f07cb85509e88e083eb5ace6e
sysoev84l <sysoev84l@ya.ru>
2024-07-21 15:43:53 +0300

    v 1.0.1

--- mybackup.sql
+++ mybackup.sql
@@ -25,7 +25,7 @@ CREATE TABLE `work_time` (
   `break_time` float NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
   UNIQUE KEY `date` (`date`)
-) ENGINE=InnoDB AUTO_INCREMENT=537 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=538 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 
 INSERT INTO work_time VALUES
@@ -530,6 +530,7 @@ INSERT INTO work_time VALUES
 ("532","2024-07-16","08:00:00","18:00:00","0"),
 ("533","2024-07-17","08:00:00","18:00:00","0"),
 ("534","2024-07-18","08:00:00","18:00:00","0"),
-("535","2024-07-19","08:00:00","18:00:00","0");
+("535","2024-07-19","08:00:00","18:00:00","0"),
+("537","2024-07-21","10:00:00","16:00:00","0");
 
 
